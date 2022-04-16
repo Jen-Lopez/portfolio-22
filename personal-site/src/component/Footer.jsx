c3a45c46bebad6707484ebe12f07c8f19c9fb818
Jen-Lopez <jennlolopz@gmail.com>
2022-04-15 21:44:57 -0400

    fix styling, add footer

---
+++ personal-site/src/component/Footer.jsx
@@ -0,0 +1,26 @@
+import './Footer.scss';
+import GH from '../img/github.svg'
+import LI from '../img/linkedin.svg'
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+        <footer className='footer'>
+            <div className='waves'>
+                <div className='wave wave1'></div>
+                <div className='wave wave2'></div>
+                <div className='wave wave3'></div>
+                <div className='wave wave4'></div>
+            </div>
+            <div className='info'>
+                <p>Made by Jen, &copy; 2022</p>
+                <div className='socials'>
+                    <a href='https://github.com/Jen-Lopez' target={'_blank'} rel="noopener noreferrer"><img src={GH} alt='github icon'/></a>
+                    <a href='https://www.linkedin.com/in/jen-lopez/' target={'_blank'} rel="noopener noreferrer"><img src={LI} alt='linkedin icon'/></a>
+                </div>
+            </div>
+        </footer>
+    </>
+  )
+}
--- personal-site/src/pages/Home.jsx
+++ personal-site/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import './Home.scss';
 import React from 'react'
 import NavBar from '../component/Navbar'
 import Header from '../component/Header';
+import Footer from '../component/Footer';
 
 function Home() {
   return (
    <>
     <NavBar/>
     <Header/>
+    <Footer/>
    </>
   )
 }
